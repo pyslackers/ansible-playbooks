ad326d464dbf3270e0f3b59d9fb153239298fe44
Ovv <contact@ovv.wtf>
2018-01-24 14:29:13 +0100

    Fix deploy 3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 echo "Preparing for deploy"
 pip install ansible
 
-openssl openssl aes-256-cbc -K $encrypted_8e30f5458f7c_key -iv $encrypted_8e30f5458f7c_iv -in id_rsa.enc -out id_rsa -d
+openssl aes-256-cbc -K $encrypted_8e30f5458f7c_key -iv $encrypted_8e30f5458f7c_iv -in id_rsa.enc -out id_rsa -d
 chmod 600 id_rsa
 
 echo -n "$ANSIBLE_PASSWORD" > .pass
